3d7e11bb90c662d7bbddb6cd3aa6360400b8e8c4
rahulXs <rahulsharmaacn@gmail.com>
2018-07-13 19:16:23 +0530

    Update files

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route('/another')
+@app.route('/about')
 def another():
-    return 'another page'
+    return render_template('about.html')
 
 if __name__=="__main__":
     app.run(debug=True, port=5001)
